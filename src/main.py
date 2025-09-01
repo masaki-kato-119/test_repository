0a50024607dce3c16760de3f0f86af5f96ca3a54
masaki kato <masaki-kato@mamezou.com>
2025-09-01 11:05:20 +0900

    スタイル変更

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 def add(a: int, b: int) -> int:
     """2つの整数を加算して返すサンプル関数"""
     return a + b
+
+def main():
+    sum = add(1,2)
+    print(f"1 + 2 = {sum}")
+
+if __name__ == "__main__":
+    main()
